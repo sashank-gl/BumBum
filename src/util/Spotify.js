f837b040b87ebcf256ae4a88fad413c382dfcb54
sashank-gl <glsashank365@gmail.com>
2022-11-27 21:00:53 +0530

    Nothing

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,5 +1,5 @@
 const clientId = process.env.REACT_APP_SPOTIFY_TOKEN;
-//clientId = e1d21d07595f43789b7be27ad0b79f85
+//const clientId = 'e1d21d07595f43789b7be27ad0b79f85';
 //const redirectUri = "http://localhost:3000";
 const redirectUri = "http://bumbumspotify.netlify.app/";
 
